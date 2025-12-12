bd0998188b49612808bc10fa95a14883338fce83
jazzygee36 <jazzygee36@gmail.com>
2025-12-12 16:05:04 +0100

    api working

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express();
-let cachedApp: any;
 
-export default async function handler(
-  req: express.Request,
-  res: express.Response,
-) {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'https://trustsuccessfinances.com',
+      'https://www.trustsuccessfinances.com',
+      'http://localhost:5173',
+    ],
 
-    // Dynamically set allowed origins based on environment
-    const allowedOrigins = [
-      'http://localhost:5173', // Development frontend URL
-      'https://trustwalletbanking.netlify.app', // Production frontend URL
-    ];
-
-    app.enableCors({
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
-
-    await app.init();
-    cachedApp = server;
-  }
-  cachedApp(req, res);
+    // Replace with your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to allow cookies
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
+bootstrap();
 
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import express from 'express';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-
-// const server = express();
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-//   // Enable CORS
-//   app.enableCors({
-//     origin: 'http://localhost:3000', // Replace with your frontend's URL
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true, // If you need to allow cookies
-//   });
-//   await app.listen(process.env.PORT ?? 4000);
-// }
-// bootstrap();
-
-// export default server;
+export default server;
